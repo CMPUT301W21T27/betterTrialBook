87bbe075041d8f38a9011585ac94d5e74173e7e6
WingWing87 <78831774+WingWing87@users.noreply.github.com>
2021-03-02 22:28:20 -0700

    Update the readability.

--- code/app/src/main/java/com/example/bettertrialbook/MainActivity.java
+++ code/app/src/main/java/com/example/bettertrialbook/MainActivity.java
@@ -4,10 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
@@ -17,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.home_screen);
 
         Spinner spinner = findViewById(R.id.SortMethod);
+        
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.method, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
