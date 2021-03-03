1d6f6a6fd6ad642dcbed2871fa00dc2e8e4e5dd8
osmani2 <55111012+osmani2@users.noreply.github.com>
2021-03-03 15:10:58 -0700

    Created owner profile layout

--- code/app/src/main/java/com/example/bettertrialbook/ProfileViewActivity.java
+++ code/app/src/main/java/com/example/bettertrialbook/ProfileViewActivity.java
@@ -10,5 +10,10 @@ public class ProfileViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+
+        int ID = 1234;
+        String username = "";
+        String email = "";
+        String phone = "";
     }
 }
