7524f5b7ed83f92b8dcbd6cf599dc77e051edcfa
osmani2 <55111012+osmani2@users.noreply.github.com>
2021-03-04 13:28:14 -0700

    Create conatctInfo class

--- code/app/src/main/java/com/example/bettertrialbook/models/ContactInfo.java
+++ code/app/src/main/java/com/example/bettertrialbook/models/ContactInfo.java
@@ -1,4 +1,27 @@
 package com.example.bettertrialbook.models;
 
 public class ContactInfo {
+    private String email;
+    private String phone;
+
+    public ContactInfo() {
+        email = " ";
+        phone = " ";
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
