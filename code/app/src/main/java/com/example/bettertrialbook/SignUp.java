2b693615090dfe04d255a455ddb49cb6bdb66144
osmani2 <55111012+osmani2@users.noreply.github.com>
2021-03-04 11:59:53 -0700

    Added button methods
    added methods to deal with button clicks.

--- code/app/src/main/java/com/example/bettertrialbook/SignUp.java
+++ code/app/src/main/java/com/example/bettertrialbook/SignUp.java
@@ -5,23 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class SignUp extends AppCompatActivity {
+
+    private String username;
+    private String email;
+    private String phone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_information);
 
-        Button cancel = findViewById(R.id.Cancel);
+    }
 
-        // Send you back to the non-user profile page
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+    public void cancelPressed(View view){
+        finish();
+    }
+
+    public void confirmPressed(View view){
+        TextView input;
+        boolean isValid=false;
+
+        input = (TextView) findViewById(R.id.Email);
+        email = input.getText().toString();
+
+        input = (TextView) findViewById(R.id.Contact);
+        phone = input.getText().toString();
+
+        //Keep looping till valid username is inputted
+        while(!isValid){
+            input = (TextView) findViewById(R.id.Username);
+            username = input.getText().toString();
+
+            //check if username is in database
+            //if not
+            isValid=true;
+        }
+
 
     }
 }
