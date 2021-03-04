4ec1cd086323a401c2cb9d68862361d711a86431
osmani2 <55111012+osmani2@users.noreply.github.com>
2021-03-04 15:15:41 -0700

    View other profiles layout

---
+++ code/app/src/main/java/com/example/bettertrialbook/ViewOthersProfile.java
@@ -0,0 +1,14 @@
+package com.example.bettertrialbook;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewOthersProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_view_others);
+    }
+}
