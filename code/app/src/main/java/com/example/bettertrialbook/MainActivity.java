8737cc93ab06039d6d58e7fab1511253148db28d
osmani2 <nahmed2@ualberta.ca>
2021-03-09 10:16:55 -0700

    Cleaned up MainActivity

--- code/app/src/main/java/com/example/bettertrialbook/MainActivity.java
+++ code/app/src/main/java/com/example/bettertrialbook/MainActivity.java
@@ -6,22 +6,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 
 import com.example.bettertrialbook.dal.UserDAL;
 import com.example.bettertrialbook.models.User;
 
-import java.io.File;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     UserDAL uDAL;
     User you;
@@ -34,16 +27,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
         generateID();
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
     }
 
     public void viewYourProfile(View view){
