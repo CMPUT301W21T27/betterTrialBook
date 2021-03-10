e3682eea14a0c6ed91494663f732ca59dcb7a315
Shasta Johnsen-Sollos <s.johnsensollos@gmail.com>
2021-03-09 18:51:48 -0700

    delete import

--- code/app/src/main/java/com/example/bettertrialbook/MainActivity.java
+++ code/app/src/main/java/com/example/bettertrialbook/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.Telephony;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
