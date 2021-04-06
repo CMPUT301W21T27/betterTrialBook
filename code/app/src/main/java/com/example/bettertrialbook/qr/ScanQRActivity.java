27a7016148b7e1234604f6e2fed3c7f52948860c
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2021-04-06 13:58:26 -0600

    access camera

--- code/app/src/main/java/com/example/bettertrialbook/qr/ScanQRActivity.java
+++ code/app/src/main/java/com/example/bettertrialbook/qr/ScanQRActivity.java
@@ -1,17 +1,58 @@
 package com.example.bettertrialbook.qr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.os.Bundle;
 
 import com.example.bettertrialbook.R;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
 
 public class ScanQRActivity extends AppCompatActivity {
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
-//    https://developer.android.com/training/camera/cameradirect
+   //https://developer.android.com/training/camerax/preview
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qr);
+
+        setupCamera();
+    }
+
+    private void setupCamera(){
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        PreviewView previewView = findViewById(R.id.camera_preview);
+        preview.setSurfaceProvider(previewView.createSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
     }
 }
