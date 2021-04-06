db9965866b78fa273d6952100f8ba6b1bf52279b
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2021-04-06 15:20:19 -0600

    uses mlkit to scan qr codes

--- code/app/src/main/java/com/example/bettertrialbook/qr/ScanQRActivity.java
+++ code/app/src/main/java/com/example/bettertrialbook/qr/ScanQRActivity.java
@@ -3,10 +3,10 @@ package com.example.bettertrialbook.qr;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -14,9 +14,7 @@ import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -24,14 +22,22 @@ import com.example.bettertrialbook.R;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
+/**
+ * Uses device camera to scan qr/bar codes.
+ * A preview of the camera's output is displayed on the screen.
+ */
 public class ScanQRActivity extends AppCompatActivity {
+    private static final String tag = "QR Scanner";
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private ActivityResultLauncher<String> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-                Log.d("QR", "Permission granted");
+                Log.d(tag, "Permission granted");
                 setupCamera();
             });
+
+    private Camera camera;
 
     //https://developer.android.com/training/camerax/preview
     @Override
@@ -54,29 +60,49 @@ public class ScanQRActivity extends AppCompatActivity {
 
     private void setupCamera() {
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        Executor cameraExecutor = ContextCompat.getMainExecutor(this);
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                bindPreview(cameraProvider);
+                bind(cameraProvider);
             } catch (ExecutionException | InterruptedException e) {
                 // No errors need to be handled for this Future.
                 // This should never be reached.
             }
-        }, ContextCompat.getMainExecutor(this));
+
+        }, cameraExecutor);
     }
 
-    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
-        Log.d("QR", "binding camera");
-        Preview preview = new Preview.Builder()
-                .build();
-
+    void bind(@NonNull ProcessCameraProvider cameraProvider) {
+        Log.d(tag, "binding camera");
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
 
-        PreviewView previewView = findViewById(R.id.camera_preview);
-        preview.setSurfaceProvider(previewView.createSurfaceProvider());
-
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+        camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, cameraPreview(), barcodeAnalyzer());
     }
+
+    private Preview cameraPreview() {
+        PreviewView previewView = findViewById(R.id.camera_preview);
+        Preview preview = new Preview.Builder()
+                .build();
+        preview.setSurfaceProvider(previewView.createSurfaceProvider());
+        return preview;
+    }
+
+    private ImageAnalysis barcodeAnalyzer() {
+        //    https://developers.google.com/ml-kit/vision/barcode-scanning/android#java
+        ImageAnalysis imageAnalysis = new ImageAnalysis
+                .Builder()
+                .build();
+        imageAnalysis
+                .setAnalyzer(
+                        ContextCompat.getMainExecutor(this),
+                        new BarcodeAnalyzer(
+                                barcode -> Log.d(tag, "Scanned the following barcode: " + barcode.getRawValue())
+                        )
+                );
+        return imageAnalysis;
+    }
+
 }
