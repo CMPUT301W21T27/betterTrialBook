570bf22fad0a2e1673807f8649b6fd7cb5dcf050
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2021-04-06 15:30:41 -0600

    create qr activity initialized

---
+++ code/app/src/main/java/com/example/bettertrialbook/qr/CreateQRActivity.java
@@ -0,0 +1,16 @@
+package com.example.bettertrialbook.qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bettertrialbook.R;
+
+public class CreateQRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_qr);
+    }
+}
