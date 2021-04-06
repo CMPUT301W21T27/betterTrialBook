676b1f02e87ed4669357dc9b34bd6f338880c5bb
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2021-04-06 17:56:27 -0600

    implemented qrdal

--- code/app/src/main/java/com/example/bettertrialbook/dal/QRDAL.java
+++ code/app/src/main/java/com/example/bettertrialbook/dal/QRDAL.java
@@ -4,5 +4,32 @@ Currently not implemented.
 
 package com.example.bettertrialbook.dal;
 
+import android.util.Log;
+
+import com.example.bettertrialbook.models.QRCode;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class QRDAL {
+    private static final String TAG = "QRDAL";
+    FirebaseFirestore db = Firestore.getInstance();
+    CollectionReference collRef = db.collection("QRCodes");
+
+    public void registerQRCode(QRCode qrCode, Runnable onSuccess) {
+        collRef.document(qrCode.getId())
+                .set(qrCode)
+                .addOnSuccessListener(aVoid -> onSuccess.run());
+    }
+
+    public void addQRCodeListener(String id, Callback<QRCode> onSuccess, Runnable onFailure) {
+        collRef.document(id).addSnapshotListener((value, error) -> {
+            if (error != null) {
+                Log.w(TAG, "Error getting qrCode" + error.toString());
+                onFailure.run();
+                return;
+            }
+            QRCode qrCode = value.toObject(QRCode.class);
+            onSuccess.execute(qrCode);
+        });
+    }
 }
--- code/app/src/main/java/com/example/bettertrialbook/models/QRCode.java
+++ code/app/src/main/java/com/example/bettertrialbook/models/QRCode.java
@@ -4,5 +4,40 @@ Currently not implemented
 
 package com.example.bettertrialbook.models;
 
+import android.os.Parcelable;
+
 public class QRCode {
+    private String experimentId;
+    private String trialId;
+    private String id;
+
+    public QRCode(String experimentId, String trialId, String id) {
+        this.experimentId = experimentId;
+        this.trialId = trialId;
+        this.id = id;
+    }
+
+    public String getExperimentId() {
+        return experimentId;
+    }
+
+    public void setExperimentId(String experimentId) {
+        this.experimentId = experimentId;
+    }
+
+    public String getTrialId() {
+        return trialId;
+    }
+
+    public void setTrialId(String trialId) {
+        this.trialId = trialId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
